e5b7dfb831a7f2bc64ecd1e73be378bb7365d11a
sameyr <ss1366t@gre.ac.uk>
2023-11-12 13:51:23 +0000

    Funcationality added : not complete

--- tic-tac.js
+++ tic-tac.js
@@ -21,9 +21,37 @@ class Game
         makeMove(i){
             if (this.board[i] == null){ 
                 this.board[i] = this.turn;
-                }
-                else{return;}
+                let winningCombination =  this.checkWinningCombination();
+
             }
+            else{return;}
+        }
+
+        checkWinningCombination(){ 
+            const winningCombination = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,4,8],
+                [2,4,6],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8]
+            ]
+
+            for (const combination of winningCombination){
+                
+                const [a,b,c] = combination; // breaking down the array and saving it into a,b and c
+                if(this.board[a] && (this.board[a] === this.board[b]) && (this.board[a] === this.board[c]) ){
+                    if (this.board[a]==="X"){console.log("Player X win")}
+                    else{console.log("player Y Wins")}
+                }
+            }
+        }
+
+
+
+
 
     }
 
@@ -35,9 +63,8 @@ class GameView
 
         updateBoard(game) {
             for(let i = 0  ; i < 9 ; i++){ 
-                const tile = document.querySelector(`.division[data-index = '${i}']`)  
+                const tile = document.querySelector(`.division[data-index = '${i}']`) 
                 tile.textContent = game.board[i];
-
             }
         }
 
@@ -46,8 +73,10 @@ class GameView
 let game = new Game; 
 let gameview = new GameView;
 
-let block = document.querySelectorAll(".division")
+let block = document.querySelectorAll(".division"); //grab all the block index from the board and saves it in "block" variable  
 
+
+// adding a event listner onClick to invoke a function to print "X" or "O" in the board 
 block.forEach((block) =>  {
     block.addEventListener("click", () => {
     onTileClicked(block.dataset.index);     
